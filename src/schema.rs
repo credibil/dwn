5e1754f185ba2bc8c114200ecf5ec5f2f6287519
andrewweston <andrew.weston@propellerhead.co.nz>
2024-10-29 13:07:16 +1300

    rename json-schemas -> schemas

--- src/schema.rs
+++ src/schema.rs
@@ -11,11 +11,11 @@ fn precompiled(schema_name: &str) -> Result<Value> {
     match schema_name {
         "protocols-configure" => {
             let schema =
-                include_bytes!("../json-schemas/interface-methods/protocols-configure.json");
+                include_bytes!("../schemas/interface-methods/protocols-configure.json");
             Ok(serde_json::from_slice(schema)?)
         }
         "protocols-query" => {
-            let schema = include_bytes!("../json-schemas/interface-methods/protocols-query.json");
+            let schema = include_bytes!("../schemas/interface-methods/protocols-query.json");
             Ok(serde_json::from_slice(schema)?)
         }
         _ => Err(anyhow!("Schema not found: {schema_name}")),
@@ -59,55 +59,55 @@ impl Retrieve for Retriever {
         match file.as_str() {
             "protocol-definition.json" => {
                 let schema =
-                    include_bytes!("../json-schemas/interface-methods/protocol-definition.json");
+                    include_bytes!("../schemas/interface-methods/protocol-definition.json");
                 Ok(serde_json::from_slice(schema)?)
             }
             "records-write-data-encoded.json" => {
                 let schema = include_bytes!(
-                    "../json-schemas/interface-methods/records-write-data-encoded.json"
+                    "../schemas/interface-methods/records-write-data-encoded.json"
                 );
                 Ok(serde_json::from_slice(schema)?)
             }
             "protocol-rule-set.json" => {
                 let schema =
-                    include_bytes!("../json-schemas/interface-methods/protocol-rule-set.json");
+                    include_bytes!("../schemas/interface-methods/protocol-rule-set.json");
                 Ok(serde_json::from_slice(schema)?)
             }
             "records-write-unidentified.json" => {
                 let schema = include_bytes!(
-                    "../json-schemas/interface-methods/records-write-unidentified.json"
+                    "../schemas/interface-methods/records-write-unidentified.json"
                 );
                 Ok(serde_json::from_slice(schema)?)
             }
 
             "defs.json" => {
-                let schema = include_bytes!("../json-schemas/definitions.json");
+                let schema = include_bytes!("../schemas/definitions.json");
                 Ok(serde_json::from_slice(schema)?)
             }
 
             "authorization.json" => {
-                let schema = include_bytes!("../json-schemas/authorization.json");
+                let schema = include_bytes!("../schemas/authorization.json");
                 Ok(serde_json::from_slice(schema)?)
             }
             "authorization-owner.json" => {
-                let schema = include_bytes!("../json-schemas/authorization-owner.json");
+                let schema = include_bytes!("../schemas/authorization-owner.json");
                 Ok(serde_json::from_slice(schema)?)
             }
             "authorization-delegated-grant.json" => {
-                let schema = include_bytes!("../json-schemas/authorization-delegated-grant.json");
+                let schema = include_bytes!("../schemas/authorization-delegated-grant.json");
                 Ok(serde_json::from_slice(schema)?)
             }
             "general-jws.json" => {
-                let schema = include_bytes!("../json-schemas/general-jws.json");
+                let schema = include_bytes!("../schemas/general-jws.json");
                 Ok(serde_json::from_slice(schema)?)
             }
 
             "public-jwk.json" => {
-                let schema = include_bytes!("../json-schemas/jwk/public-jwk.json");
+                let schema = include_bytes!("../schemas/jwk/public-jwk.json");
                 Ok(serde_json::from_slice(schema)?)
             }
             "general-jwk.json" => {
-                let schema = include_bytes!("../json-schemas/jwk/general-jwk.json");
+                let schema = include_bytes!("../schemas/jwk/general-jwk.json");
                 Ok(serde_json::from_slice(schema)?)
             }
 
