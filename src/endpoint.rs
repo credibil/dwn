c6311ed43dee49a834a222b4ac200bb1edfed80d
andrewweston <andrew.weston@propellerhead.co.nz>
2025-02-01 14:48:28 +1300

    endpoint docs

--- src/endpoint.rs
+++ src/endpoint.rs
@@ -15,7 +15,13 @@ use crate::{Descriptor, Result, schema, unauthorized};
 /// Handle incoming messages.
 ///
 /// # Errors
-/// LATER: Add errors
+///
+/// This method can fail for a number of reasons related to the imcoming
+/// message's viability. Expected failues include invalid authorization,
+/// insufficient permissions, and invalid message content.
+///
+/// Implementers should look to the Error type and description for more
+/// information on the reason for failure.
 pub async fn handle<T>(
     owner: &str, message: impl Message<Reply = T>, provider: &impl Provider,
 ) -> Result<Reply<T>> {
@@ -24,29 +30,36 @@ pub async fn handle<T>(
 }
 
 /// Methods common to all messages.
+///
+/// The primary role of this trait is to provide a common interface for
+/// messages so they can be handled by `[handle]` method.
 pub trait Message: Serialize + Clone + Debug + Send + Sync {
-    /// The message's inner reply type.
+    /// The inner reply type specific to the implementing message.
     type Reply;
 
-    /// Compute the CID of the message.
+    /// Compute the content identifier (CID) for the message.
     ///
     /// # Errors
-    /// LATER: Add errors
+    /// 
+    /// This method can fail if the message cannot be serialized to CBOR.
     fn cid(&self) -> Result<String>;
 
-    /// Returns the component of the message descriptor common to all messages
+    /// Returns message descriptor properties common to all messages (i.e., 
+    /// `interface`, `method`, and `message_timestamp`).
     fn descriptor(&self) -> &Descriptor;
 
-    /// Returns the messages's authorization, if set.
+    /// Returns the messages's authorization, when set.
     fn authorization(&self) -> Option<&Authorization>;
 
-    /// Handle the message.
+    /// Routes the message to the concrete handler used to process the message.
     fn handle(
         self, owner: &str, provider: &impl Provider,
     ) -> impl Future<Output = Result<Reply<Self::Reply>>> + Send;
 
-    /// Validate the message. This is a generic validation common to all messages.
-    /// Message-specific validation is done in the message handler.
+    /// Perform initial validation of the message. 
+    /// 
+    /// Validation undertaken here is common to all messages, with message-
+    /// specific validation performed by the message's handler.
     fn validate(
         &self, _owner: &str, provider: &impl Provider,
     ) -> impl Future<Output = Result<()>> + Send {
@@ -70,13 +83,13 @@ pub trait Message: Serialize + Clone + Debug + Send + Sync {
     }
 }
 
-/// Reply used by all endpoints.
+/// Top-level reply data structure common to all handler.
 #[derive(Debug, Default, Deserialize, Serialize)]
 pub struct Reply<ReplyBody> {
-    /// Status message to accompany the reply.
+    /// The status message to accompany the reply.
     pub status: Status,
 
-    /// Endpoint-specific reply.
+    /// The endpoint-specific reply.
     #[serde(skip_serializing_if = "Option::is_none")]
     #[serde(flatten)]
     pub body: Option<ReplyBody>,
