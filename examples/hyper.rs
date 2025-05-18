a7be286c8e6741103acbd3e5156b1ea4c83c257a
andrewweston <andrew.weston@propellerhead.co.nz>
2025-05-18 13:51:33 +1200

    process json-rpc

--- examples/hyper.rs
+++ examples/hyper.rs
@@ -1,13 +1,15 @@
 //! A simple Hyper HTTP server that handles DWN messages.
 
-use anyhow::Result;
-use credibil_dwn::interfaces::{messages, protocols, records};
+use anyhow::{Result, anyhow};
+use credibil_dwn::interfaces::{Descriptor, messages, protocols, records};
 use credibil_dwn::{self, IntoHttp};
 use http_body_util::{BodyExt, Full};
 use hyper::body::{Bytes, Incoming};
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper_util::rt::TokioIo;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 use test_utils::{Identity, ProviderImpl};
 use tokio::net::TcpListener;
 
@@ -45,50 +47,103 @@ impl Svc {
     // Handle DWN messages.
     async fn handle(&self, req: hyper::Request<Incoming>) -> Result<hyper::Response<Full<Bytes>>> {
         let path = req.uri().path().to_string();
-        let body = req.into_body().collect().await.expect("should have body");
 
         match path.as_str() {
-            "/messages/query" => {
-                let request = serde_json::from_slice::<messages::Query>(&body.to_bytes())?;
-                Ok(credibil_dwn::handle(&self.owner, request, &self.provider).await.into_http())
-            }
-            "/messages/read" => {
-                let request = serde_json::from_slice::<messages::Read>(&body.to_bytes())?;
-                Ok(credibil_dwn::handle(&self.owner, request, &self.provider).await.into_http())
-            }
-            "/messages/subscribe" => {
-                let request = serde_json::from_slice::<messages::Subscribe>(&body.to_bytes())?;
-                Ok(credibil_dwn::handle(&self.owner, request, &self.provider).await.into_http())
-            }
-            "/protocols/configure" => {
-                let request = serde_json::from_slice::<protocols::Configure>(&body.to_bytes())?;
-                Ok(credibil_dwn::handle(&self.owner, request, &self.provider).await.into_http())
-            }
-            "/protocols/query" => {
-                let request = serde_json::from_slice::<protocols::Query>(&body.to_bytes())?;
-                Ok(credibil_dwn::handle(&self.owner, request, &self.provider).await.into_http())
-            }
-            "/records/delete" => {
-                let request = serde_json::from_slice::<records::Delete>(&body.to_bytes())?;
-                Ok(credibil_dwn::handle(&self.owner, request, &self.provider).await.into_http())
-            }
-            "/records/query" => {
-                let request = serde_json::from_slice::<records::Query>(&body.to_bytes())?;
-                Ok(credibil_dwn::handle(&self.owner, request, &self.provider).await.into_http())
-            }
-            "/records/read" => {
-                let request = serde_json::from_slice::<records::Read>(&body.to_bytes())?;
-                Ok(credibil_dwn::handle(&self.owner, request, &self.provider).await.into_http())
-            }
-            "/records/subscribe" => {
-                let request = serde_json::from_slice::<records::Subscribe>(&body.to_bytes())?;
-                Ok(credibil_dwn::handle(&self.owner, request, &self.provider).await.into_http())
-            }
-            "/records/write" => {
-                let request = serde_json::from_slice::<records::Write>(&body.to_bytes())?;
-                Ok(credibil_dwn::handle(&self.owner, request, &self.provider).await.into_http())
+            "/" => self.process_message(req).await,
+            "/health" => {
+                let body = serde_json::to_vec(&json!({
+                    "status": "ok",
+                    "version": "0.1.0",
+                }))?;
+                Ok(hyper::Response::new(Full::from(body)))
             }
             _ => Err(anyhow::anyhow!("path {path} is not supported")),
         }
     }
+
+    async fn process_message(
+        &self, req: hyper::Request<Incoming>,
+    ) -> Result<hyper::Response<Full<Bytes>>> {
+        // process body into json-rpc
+        let body = req.into_body().collect().await?.to_bytes();
+        let rpc = serde_json::from_slice::<JsonRpc>(&body)?;
+
+        // partially parse the message to determine the interface and method
+        let Some(value) = rpc.params.message.get("descriptor") else {
+            return Err(anyhow!("message has noo descriptor "));
+        };
+        let desc = serde_json::from_value::<Descriptor>(value.clone())?;
+
+        match format!("{}{}", desc.interface, desc.method).as_str() {
+            "MessagesQuery" => {
+                let request = serde_json::from_value::<messages::Query>(rpc.params.message)?;
+                Ok(credibil_dwn::handle(&self.owner, request, &self.provider).await.into_http())
+            }
+            "MessagesRead" => {
+                let request = serde_json::from_value::<messages::Read>(rpc.params.message)?;
+                Ok(credibil_dwn::handle(&self.owner, request, &self.provider).await.into_http())
+            }
+            "MessagesSubscribe" => {
+                let request = serde_json::from_value::<messages::Subscribe>(rpc.params.message)?;
+                Ok(credibil_dwn::handle(&self.owner, request, &self.provider).await.into_http())
+            }
+            "ProtocolsConfigure" => {
+                let request = serde_json::from_value::<protocols::Configure>(rpc.params.message)?;
+                Ok(credibil_dwn::handle(&self.owner, request, &self.provider).await.into_http())
+            }
+            "ProtocolsQuery" => {
+                let request = serde_json::from_value::<protocols::Query>(rpc.params.message)?;
+                Ok(credibil_dwn::handle(&self.owner, request, &self.provider).await.into_http())
+            }
+            "RecordsDelete" => {
+                let request = serde_json::from_value::<records::Delete>(rpc.params.message)?;
+                Ok(credibil_dwn::handle(&self.owner, request, &self.provider).await.into_http())
+            }
+            "RecordsQuery" => {
+                let request = serde_json::from_value::<records::Query>(rpc.params.message)?;
+                Ok(credibil_dwn::handle(&self.owner, request, &self.provider).await.into_http())
+            }
+            "RecordsRead" => {
+                let request = serde_json::from_value::<records::Read>(rpc.params.message)?;
+                Ok(credibil_dwn::handle(&self.owner, request, &self.provider).await.into_http())
+            }
+            "RecordsSubscribe" => {
+                let request = serde_json::from_value::<records::Subscribe>(rpc.params.message)?;
+                Ok(credibil_dwn::handle(&self.owner, request, &self.provider).await.into_http())
+            }
+            "RecordsWrite" => {
+                let request = serde_json::from_value::<records::Write>(rpc.params.message)?;
+                Ok(credibil_dwn::handle(&self.owner, request, &self.provider).await.into_http())
+            }
+            _ => Err(anyhow!("{}{} is invalid", desc.interface, desc.method)),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct JsonRpc {
+    /// A string with the name of the method to be invoked.
+    /// Should be `dwn.processMessage`
+    method: String,
+
+    /// An object or array of values to be passed as parameters to the defined method.
+    params: Params,
+
+    /// Used to match the response with the request that it is replying to.
+    id: String,
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct Params {
+    /// The DID that the message is intended for
+    target: String,
+
+    /// The DWN Message
+    message: serde_json::Value,
+
+    /// Data associated with the message (e.g. `RecordsWrite` encoded data)
+    #[serde(skip_serializing_if = "Option::is_none")]
+    encoded_data: Option<String>,
 }
